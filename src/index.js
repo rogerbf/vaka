5399ce562d96ed612eefa38105ccbf68ca3bfc06
rogerbf <roger@bergfalk.nu>
2016-09-16 22:47:22 +0200

    basic structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-module.exports = {}
+const caffeinate = require('caffeinate')
+const pmset = require('noidle')
+const chooseMethod = require('./lib/chooseMethod.js')
+
+const vaka = () => {}
+
+module.exports = vaka
